38c8608d7ab791f5f508de767877018d28d599e2
mr-Vasyl <gerchak9999@gmail.com>
2020-02-17 21:34:24 +0200

    Updates

---
+++ precache-manifest.115618e2848f8836aca50c5b5f4cbaf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95cb79c3ad33170d14f81cd91c1b1f8c",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "9a1fc3758a98787625f8",
+    "url": "/store/static/css/main.7bc6956f.chunk.css"
+  },
+  {
+    "revision": "1bb77888ccd65993210c",
+    "url": "/store/static/js/2.3eb5ed5d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/store/static/js/2.3eb5ed5d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a1fc3758a98787625f8",
+    "url": "/store/static/js/main.4a670d2b.chunk.js"
+  },
+  {
+    "revision": "fce600e6420b6cabb48a",
+    "url": "/store/static/js/runtime-main.9e69b2a3.js"
+  }
+]);
