69a79460a7abfcc31ef7335952bb2cb6c9fb10e0
mr-Vasyl <gerchak9999@gmail.com>
2020-02-18 02:39:29 +0200

    changed links to pictures

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -5,13 +5,13 @@ export default class BookstoreService {
             title: 'Production-Ready Microservices',
             author: 'Susan J. Fowler',
             price: 32,
-            coverImage: 'https://images-na.ssl-images-amazon.com/images/I/51FHuacxYjL._SX379_BO1,204,203,200_.jpg'},
+            coverImage: 'https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg'},
         {
             id: 2,
             title: 'Release It!',
             author: 'Michael T. Nygard',
             price: 45,
-            coverImage: 'https://images-na.ssl-images-amazon.com/images/I/51lu4ArIFYL._SX379_BO1,204,203,200_.jpg'}
+            coverImage: 'https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg'}
     ];
 
     getBooks() {
